64cc4948e0fc1ebb76514eaf56ad6bcddb858a56
Litovkasvjob <Litovkasvjob@gmail.com>
2025-04-06 23:04:21 +0200

    chore: initialize project structure
    Set up the initial project structure with the following:
    - Base directory layout
    - Dependencies configuration
    - Essential files (e.g., README, .gitignore)

---
+++ src/test/java/com/litovka/project_example/ProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litovka.project_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
